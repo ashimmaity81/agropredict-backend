d4737bb9ed9b2d168edef97afaea33656c5084b7
ashimmaity81 <maityashim81@gmail.com>
2026-05-03 04:46:39 +0530

    done

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # -----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://agropredict06.netlify.app","https://agropredict06lpu.netlify.app/"],
+    allow_origins=["https://agropredict06.netlify.app","https://agropredict06lpu.netlify.app","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
